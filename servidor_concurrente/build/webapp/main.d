f8162cc1a2b15541957874c4acc916efe38ab3d6
EstebanRojas521 <estrojas521@gmail.com>
2022-10-15 16:34:36 -0600

    Preparando formato HTML para impresion de numeros Goldbach

--- servidor_concurrente/build/webapp/main.d
+++ servidor_concurrente/build/webapp/main.d
@@ -7,4 +7,4 @@ build/webapp/main.o: src/webapp/main.cpp src/http/HttpServer.hpp \
  src/prodcons/Queue.hpp src/http/HttpServer.hpp src/http/HttpApp.hpp \
  src/http/HttpPackage.hpp src/webapp/FactWebApp.hpp \
  src/webapp/GoldbachWebApp.hpp src/http/HttpApp.hpp \
- src/http/HttpPackage.hpp src/webapp/GoldbachResponder.hpp
+ src/http/HttpPackage.hpp src/webapp/GoldbachHTML.hpp
--- servidor_concurrente/src/http/HttpPackage.cpp
+++ servidor_concurrente/src/http/HttpPackage.cpp
@@ -1,17 +1,10 @@
-
-
 #include "HttpPackage.hpp"
 
 HttpPackage::HttpPackage(const HttpRequest& httpRequest,
-  const HttpResponse& httpResponse, bool stopCondition)
-  : httpRequest(httpRequest), httpResponse(httpResponse)
-  , condicionParada(condicionParada) {
+  const HttpResponse& httpResponse)
+  : httpRequest(httpRequest), httpResponse(httpResponse){
     this->solicitudInvalida = false;
 }
 
 HttpPackage::~HttpPackage() {
-}
-
-bool HttpPackage::operator==(const HttpPackage httpPackage) const {
-  return (this->condicionParada == httpPackage.condicionParada);
 }
