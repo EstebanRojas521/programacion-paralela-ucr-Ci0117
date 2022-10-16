a85ee209c94a28ddb1775879993a26563510a1ca
ulisesfh02 <jose.fonsecahurtado@ucr.ac.cr>
2022-10-16 01:50:49 -0600

    Se eliminan archivos de carpeta bin y build

--- servidor_concurrente/build/http/HttpResponse.d
+++
@@ -1,3 +0,0 @@
-build/http/HttpResponse.o: src/http/HttpResponse.cpp \
- src/http/HttpResponse.hpp src/http/HttpMessage.hpp src/common/common.hpp \
- src/network/Socket.hpp
--- servidor_concurrente/build/http/HttpServer.d
+++
@@ -1,8 +0,0 @@
-build/http/HttpServer.o: src/http/HttpServer.cpp src/http/HttpApp.hpp \
- src/common/common.hpp src/http/HttpServer.hpp src/network/TcpServer.hpp \
- src/http/HttpRequest.hpp src/http/HttpMessage.hpp src/network/Socket.hpp \
- src/http/HttpResponse.hpp src/http/HttpConnectionHandler.hpp \
- src/prodcons/Consumer.hpp src/prodcons/Queue.hpp \
- src/prodcons/Semaphore.hpp src/prodcons/Thread.hpp \
- src/prodcons/Queue.hpp src/common/Log.hpp src/common/common.hpp \
- src/network/NetworkAddress.hpp
