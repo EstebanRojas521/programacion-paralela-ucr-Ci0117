6410c738558f3dc7aa0574933d2165df1db82adc
ulisesfh02 <jose.fonsecahurtado@ucr.ac.cr>
2022-10-16 01:58:25 -0600

    Se elimina clase GoldbachPthread

--- servidor_concurrente/build/webapp/GoldbachHTML.d
+++
@@ -1,7 +0,0 @@
-build/webapp/GoldbachHTML.o: src/webapp/GoldbachHTML.cpp \
- src/webapp/GoldbachHTML.hpp src/common/common.hpp \
- src/prodcons/Consumer.hpp src/prodcons/Queue.hpp \
- src/prodcons/Semaphore.hpp src/prodcons/Thread.hpp \
- src/http/HttpPackage.hpp src/http/HttpRequest.hpp \
- src/http/HttpMessage.hpp src/network/Socket.hpp \
- src/http/HttpResponse.hpp
