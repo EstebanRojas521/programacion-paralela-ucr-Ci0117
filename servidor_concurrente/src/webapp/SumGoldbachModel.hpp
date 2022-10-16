e8a43f576d5085e0d028432cbc8cb5efa4992e85
EstebanRojas521 <estrojas521@gmail.com>
2022-10-15 20:01:57 -0600

    Agregando pseudo golbach

---
+++ servidor_concurrente/src/webapp/SumGoldbachModel.hpp
@@ -0,0 +1,27 @@
+/*
+
+
+
+
+int main(numero, vector){
+
+    calcular_golbach_pares(numero, vector)
+    calcular_golbach_inpares(numero, vector)
+}
+
+
+calcular_golbach_pares(numero,vector){
+    si lo encuentra:
+        vector.pushback(N1)
+        vector.pushback(N2)
+}
+
+
+calcular_golbach_pares(numero,vector){
+    si lo encuentra:
+        vector.pushback(suma)
+}
+
+
+
+*/
