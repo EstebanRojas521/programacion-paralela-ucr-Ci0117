b2e06afe7c3165194d9719f72d071022ba7cd99c
YasmynCH <yasmyn_3395@hotmail.com>
2022-11-25 22:14:36 -0600

    Propuesta .h de lamina

---
+++ Proy02/src/lamina.hpp
@@ -0,0 +1,34 @@
+// Copyright 2022  Yasmyn Chacón Hernández,Ulises Fonseca Hurtado
+// y Esteban Rojas Carranza - Universidad de Costa Rica.
+
+#ifndef LAMINA_HPP
+#define LAMINA_HPP
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct lamina_t {
+    /// Cantidad de filas - R
+    uint64_t filas;
+    /// Cantidad de columnas - C
+    uint64_t columnas;
+    /// Estado - k
+    uint64_t estado;
+    /// Tiempo - t
+    uint64_t tiempo;
+    ///Tamaño (alto/ancho) - h
+    uint64_t tamanio;
+    /// Matriz
+    vector<double>matriz;
+};
+
+int read();
+void write();
+
+ifstream &operator>>(ifstream &job, lamina_t &lamina);
+ofstream &operator<<(ofstream &reporte, lamina_t &lamina);
+
+#endif
---
+++ Proy02/src/simulacion.hpp
@@ -0,0 +1,4 @@
+// Copyright 2022  Yasmyn Chacón Hernández,Ulises Fonseca Hurtado
+// y Esteban Rojas Carranza - Universidad de Costa Rica.
+
+
