5320444880ad899d157563798c2c0558a777e089
ER <estrojas521@gmail.com>
2022-12-06 06:11:37 -0600

    Programa de pruebas en MPI implementado

--- Proyecto2.2/programaPrueba/src/main.cpp
+++ Proyecto2.2/programaPrueba/src/main.cpp
@@ -2,13 +2,15 @@
 // y Esteban Rojas Carranza - Universidad de Costa Rica.
 
 #include "../../src/lamina.hpp"
+//#include "../../output/plate001-4.bin"
+//#include "../../jobs"
 #include "comparador.h"
 
 #include <string>
 
 int main(int argc, char* argv[]){
 
-    if (argc != 4) {
+    if (argc != 3) {
         std::string error = "";
         error += "usage: bin/programaPrueba ../jobs/jobxxx/jobxxx.bin";
         error += " ../jobs/jobxxx/jobxxx.bin epsilon";
@@ -22,20 +24,22 @@ int main(int argc, char* argv[]){
     txtDataA.plateName = argv[1];
     lamina_t laminaB;
     txtData txtDataB;
-    txtDataB.plateName = argv[2];
+    txtDataB.plateName = txtDataA.plateName;
+    // Buscamos nuestra plate en la carpeta de plates suministrada 
+    // por el profe, y la comparamos con el plate que obtuvimos
+    txtDataA.plateName = "../output/"+txtDataA.plateName;
+    txtDataB.plateName = "../jobs/" + txtDataB.plateName;
     laminaA = miComparador->readBinaryFile(txtDataA);
     laminaB = miComparador->readBinaryFile(txtDataB);
-
-    laminaA.epsilon = laminaB.epsilon = std::stod(argv[3]);
+    //std::cout << txtDataA.plateName << std::endl;
+    //laminaA.epsilon = laminaB.epsilon = std::stod(argv[3]);
 
     bool iguales = miComparador->compare(laminaA, laminaB);
-
     if (iguales == true) {
-        std::cout << "\nLas matrices son iguales\n";
+        std::cout << "\nLas matrices son iguales\n"<< std::endl;
     } else {
-        std::cout << "\nLas matrices son diferentes\n";
+        std::cout << "\nLas matrices son diferentes\n"<<std::endl;
     }
-
     delete miComparador;
     return 0;
 }
